36f230324954424c193dd016428345457f7c666c
Will Bridges <106503676+wbridgri@users.noreply.github.com>
2025-04-22 14:46:52 -0500

    rivals and heroes and whatnot

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios')
 
 const PORT = process.env.PORT || 3000
 
@@ -17,7 +18,8 @@ router.get('/api', (req, res) => {
 });
 
 const endpoints = [
-    'hero', 'power',
+    'hero',
+    'power',
     'species',
     'franchise',
     'team'
@@ -27,6 +29,47 @@ endpoints.forEach(endpoint => {
     router.use(`/api/${endpoint}`, require(`./api/${endpoint}Routes`))
 })
 
+//  homepage
+router.get('/', (req, res)=> {
+    //res.render(path =>where are we rendering, obj => what are we rendering)
+    res.render('pages/home', {
+        title: 'Home Page',
+        name: 'My Hero Website'
+    })
+})
 
+//  hero page => localhost:3000/heroes
+router.get('/heroes', (req, res)=> {
+    //  make our fetch call
+    const url = `http://localhost:${PORT}/api/hero`
+
+    axios.get(url).then(resp => {
+
+
+        res.render('pages/allHero', {
+            title: 'All Heroes',
+            name: 'All Heroes ...and some villains too',
+            data: resp.data
+        })
+    })
+})
+
+router.get('/heroes/:id', (req, res) =>{
+
+    const id = req.params.id
+
+    const url = `http://localhost:${PORT}/api/hero/${id}`
+
+    axios.get(url)
+        .then(resp => {
+            let heroName = resp.data.hero_name == null ? `${resp.data.first_name} ${resp.data.last_name}` : resp.data.hero_name
+
+            res.render('pages/heroSingle', {
+                title: heroName,
+                name : heroName,
+                data: resp.data
+            })
+        })
+})
 
 module.exports = router;
